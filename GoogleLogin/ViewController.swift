3c281b436541faa1762cf922b29b8b9dd0ffb4ce
sivakumar <sivainvitesyou@gmail.com>
2019-01-30 18:31:16 +0530

    Initial Commit

---
+++ GoogleLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleLogin
+//
+//  Created by TechMahindra on 30/01/19.
+//  Copyright © 2019 TechMahindra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
